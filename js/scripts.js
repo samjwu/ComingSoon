50be5d8812be02544cfd058682d2949259fc83f9
unknown <swm1000@hotmail.com>
2018-05-13 21:28:52 -0400

    release time calculations and display

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,23 @@ const releasedate = new Date("December 31, 2018 23:00:00").getTime();
 
 const update = setInterval(() => {
     const currtime = new Date().getTime();
-    const timediff = releasedate = currtime;
+    const timediff = releasedate - currtime;
+    
+    const numdays = Math.floor(timediff / (1000 * 60 * 60 * 24));
+    const numhours = Math.floor((timediff / (1000 * 60 * 60)) % 24);
+    const nummins = Math.floor((timediff / (1000 * 60)) % (60 * 24));
+    const numsecs = Math.floor((timediff / (1000)) % (60 * 60 * 24));
+
+    clock.innerHTML = `
+        <div>${numdays}<span>Days</span></div>
+        <div>${numhours}<span>Hours</span></div>
+        <div>${nummins}<span>Minutes</span></div>
+        <div>${numsecs}<span>Seconds</span></div>
+    `;
+
+    if(timediff <= 0) {
+        clearInterval(update);
+        clock.style.color = "#666";
+        clock.innerHTML = "Released";
+    }
 }, 1000);
